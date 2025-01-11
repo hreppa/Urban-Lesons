9332e70e170233dae32b39a0945d6ab4a6ddd245
Пётр <khreptugov@gmail.com>
2025-01-11 08:27:46 +0300

    конечный вид решения

--- mod_2_4.py
+++ mod_2_4.py
@@ -1,38 +1,33 @@
 def is_prime_figure(fig):
+    """
+    функция определяющая Простое число или нет
+
+    """
 
     for i in range(2, int((fig**0.5) + 1)):
-
-        if fig % i != 0:
-
+        if fig % i == 0:
+            break
         return True
-
     else:
-
         return False
 
 
-numbers = [i for i in range(1, 16)]
+numbers = [i for i in range(1, 16)] # создаем список от 1 до 15
 
 print(numbers)
 
-prim_nums = []
+prim_nums = [] # список для сбора простых чисел
 
-not_prim_nums = []
-
-
+not_prim_nums = [] # список для сбора не простых чисел
 
 for elem in numbers:
-
     if elem == 1:
-
         continue
-
-    elif is_prime_figure(elem):
-
+    elif elem == 2:
         prim_nums.append(elem)
-
+    elif is_prime_figure(elem):
+        prim_nums.append(elem)
     else:
-
         not_prim_nums.append(elem)
 
 
