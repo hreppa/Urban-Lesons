7cb62cefcb865ef19513114c721282d62205e706
@piotr_khreptugov <khreptugov@gmail.com>
2025-01-11 17:32:09 +0300

    набросок решения 18:30

--- mod_2_6.py
+++ mod_2_6.py
@@ -2,27 +2,32 @@
 
 import random
 
-def num_gen():
+def num_gen(fig):
     """
     генератор двузначного чилса
     возвращает число и сумму цифр
     ab (str), ab_sum (int)
     """
-    a = random.randint(0,10)
-    b = random.randint(0,10)
-    ab = str(a) + str(b)
-    ab_sum = a + b
-    return ab,  ab_sum
+    a = random.randint(0,fig)
+    # b = random.randint(0,10)
+    # ab = str(a) + str(b)
+    # ab_sum = a + b
+    return a
 
 def password_gen(figer):
-    # code_list = []
-    # for i in range(1, figer):
+    code_list = ''
+    for i in range(3, figer):
+        while True:
+            if figer % num_gen(figer) == 0:
+                code_list.join(num_gen())
+                break
+            continue
+    return code_list
 
-    pass
+base_number = int(input('Введите число от 3 до 20 для создания кодового числа: '))+1
 
-# base_number = int(input('Введите число от 3 до 20 на основе которого будет сгенерирована кодовая последовательность. - '))
-#
-# code_number = password_gen(base_number)
-# print(f'Кодовое число\n\t {code_number}')
+code_number = password_gen(base_number)
+print(f'Кодовое число\n\t {code_number}')
 
-num_gen()
+print(num_gen()[1], num_gen())
+print(num_gen()[0], num_gen()[1])
